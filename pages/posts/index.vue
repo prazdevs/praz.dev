104226f92c46b0b21c3aade6d66328a7e28d9cf5
PraZ <sachabouillez@gmail.com>
2022-05-24 22:11:38 +0200

    feat: use ContentQuery component

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,12 +1,6 @@
 <script setup lang="ts">
 const { formatDate } = useDate()
 
-const { data } = await useAsyncData('post-data', () =>
-  queryContent('posts')
-    .where({ _id: { $regex: 'posts:index' } })
-    .only(['title', 'description'])
-    .findOne(),
-)
 const { data: posts } = await useAsyncData('post-list', () =>
   queryContent('posts')
     .where({ _empty: { $eq: false }, _id: { $regex: /\d+.*/i } })
@@ -19,45 +13,49 @@ const { data: posts } = await useAsyncData('post-list', () =>
 <template>
   <div flex flex-col gap-7 mt-4 sm:mt-7>
     <div flex flex-col gap-4>
-      <!-- <ContentQuery v-slot="{ data }" :where="{ _id: { $regex: 'posts:index' } }"> -->
-      <h1 h1-primary mr-auto>
-        {{ data.title }}
-      </h1>
-      <p>{{ data.description }}</p>
-      <!-- </ContentQuery> -->
+      <ContentQuery
+        v-slot="{ data: { title, description } }"
+        find="one"
+        path="posts"
+        :only="['title', 'description']"
+        :where="{ _id: { $regex: 'posts:index' } }"
+      >
+        <h1 h1-primary mr-auto>
+          {{ title }}
+        </h1>
+        <p>{{ description }}</p>
+      </ContentQuery>
     </div>
     <div flex flex-col>
-      <div
-        v-for="post in posts"
-        :key="post.title"
-        flex flex-col gap-2
-        not-last:border-b-1 not-last-pb-6 not-first-pt-6
-      >
-        <h2>
-          <NuxtLink
-            :to="post._path"
-            main-link
-            font-600 text-xl
-            leading-relaxed sm:leading-loose
-          >
-            {{ post.title }}
-          </NuxtLink>
-        </h2>
-        <div flex gap-3>
-          <div flex items-center gap-1>
-            <div i-iconoir-calendar inline-flex />
-            <span>{{ formatDate(post.date) }}</span>
+      <ContentQuery>
+        <div
+          v-for="post in posts"
+          :key="post.title"
+          flex flex-col gap-2
+          not-last:border-b-1 not-last-pb-6 not-first-pt-6
+        >
+          <h2>
+            <NuxtLink
+              :to="post._path"
+              main-link
+              font-600 text-xl
+              leading-relaxed sm:leading-loose
+            >
+              {{ post.title }}
+            </NuxtLink>
+          </h2>
+          <div flex gap-3>
+            <div flex items-center gap-1>
+              <div i-iconoir-calendar inline-flex />
+              <span>{{ formatDate(post.date) }}</span>
+            </div>
           </div>
-        <!-- <div flex items-center gap-1>
-          <div i-iconoir-wristwatch inline-flex />
-          <span>{{ 'time to read' }}</span>
-        </div> -->
+          <div inline-flex gap-2 flex-wrap>
+            <TechTag v-for="tag in post.tags" :key="tag" :tech="tag" />
+          </div>
+          <p>{{ `${post.description.slice(0, 160)}…` }}</p>
         </div>
-        <div inline-flex gap-2 flex-wrap>
-          <TechTag v-for="tag in post.tags" :key="tag" :tech="tag" />
-        </div>
-        <p>{{ `${post.description.slice(0, 160)}…` }}</p>
-      </div>
+      </ContentQuery>
     </div>
   </div>
 </template>
