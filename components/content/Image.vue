b06301b39e8dba8510c3feb4cdfcec1a2eae6f9c
prazdevs <sachabouillez@gmail.com>
2022-05-27 16:33:31 +0200

    feat: add image components and refactor articles

---
+++ components/content/Image.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+withDefaults(defineProps<{ src: string; alt?: string }>(), { alt: "" });
+</script>
+
+<template>
+  <figure flex flex-col items-center justify-center my-7>
+    <img
+      w-full
+      max-w-600px
+      :src="`../content/${src}`"
+      :alt="alt"
+      loading="lazy"
+      decoding="async"
+    />
+    <figcaption italic text-sm mt-1>
+      <Markdown :use="$slots.caption" unwrap="p" />
+    </figcaption>
+  </figure>
+</template>
