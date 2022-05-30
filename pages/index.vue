75e7165e336efb149f7bb5162f865facf623f20e
prazdevs <sachabouillez@gmail.com>
2022-05-30 14:44:29 +0200

    feat: add WIP warning

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,27 @@
 <template>
-  <div flex flex-col sm:flex-row gap-4 justify-center items-center mt-7>
-    <ContentQuery v-slot="{ data }" path="home" find="one">
-      <img
-        border-2 border-primary rounded-full
-        h-7rem w-7rem
-        src="~/assets/imgs/sacha.webp"
-      >
-      <div flex flex-col items-start justify-center gap-2>
-        <h1 h1-primary>
-          {{ data.title }}
-        </h1>
-        <p text-lg>
-          {{ data.description }}
-        </p>
-      </div>
-    </ContentQuery>
+  <div flex flex-col gap-7 mt-4 sm:mt-7>
+    <div flex flex-col sm:flex-row gap-4 justify-center items-center>
+      <ContentQuery v-slot="{ data }" path="home" find="one">
+        <img
+          border-2
+          border-primary
+          rounded-full
+          h-7rem
+          w-7rem
+          src="~/assets/imgs/sacha.webp"
+        >
+        <div flex flex-col items-center sm:items-start justify-center gap-2>
+          <h1 h1-primary>
+            {{ data.title }}
+          </h1>
+          <p text-lg>
+            {{ data.description }}
+          </p>
+        </div>
+      </ContentQuery>
+    </div>
+    <div text-xl mt-8 font-bold italic text-center>
+      {{ 'I am currently rebuilding my website in Nuxt. Posts and projects are still available!' }}
+    </div>
   </div>
 </template>
