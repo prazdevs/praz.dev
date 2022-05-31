4eb01a9f55f159b1708c2b213ddb873c7d09cff0
prazdevs <sachabouillez@gmail.com>
2022-05-31 15:53:20 +0200

    feat: style inline code blocks

---
+++ components/content/ProseCodeInline.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const od = useOpenDyslexic();
+</script>
+
+<template>
+  <span
+    bg-gray-200
+    dark:bg-gray-700
+    px-1
+    rounded-sm
+    :u:font="od.openDyslexic ? 'monodyslexic' : 'mono'"
+  >
+    <Markdown :use="$slots.default" />
+  </span>
+</template>
