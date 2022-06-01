d619ecc54590b87d8d808581dd76633763e34cb0
prazdevs <sachabouillez@gmail.com>
2022-06-01 13:28:06 +0200

    feat: add ellipsis on codeblock titles

--- components/content/CodeBlock.vue
+++ components/content/CodeBlock.vue
@@ -1,15 +1,25 @@
 <script setup lang="ts">
 defineProps<{ title?: string }>();
 
-const od = useOpenDyslexic()
+const od = useOpenDyslexic();
 </script>
 
 <template>
-  <div :u:font="od.openDyslexic ? '!monodyslexic' : 'mono'" u:bg="dark:#011627" p-6 rounded-20px border-2 dark:border-0>
+  <div
+    :u:font="od.openDyslexic ? '!monodyslexic' : 'mono'"
+    u:bg="dark:#011627"
+    p-6
+    rounded-20px
+    border-2
+    dark:border-0
+  >
     <div
       v-if="title"
       mt--6
       w-fit
+      max-w-full
+      overflow-hidden
+      text-ellipsis
       px-2
       rounded-b-5px
       mb-6
