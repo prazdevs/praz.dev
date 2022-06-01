ec623a13f0cd204baf4ffa09be3c65cdc5ebb5a2
prazdevs <sachabouillez@gmail.com>
2022-06-01 13:49:20 +0200

    feat: refactor posts index to ContentList

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,13 +1,5 @@
 <script setup lang="ts">
 const { formatDate } = useDate()
-
-const { data: posts } = await useAsyncData('post-list', () =>
-  queryContent('posts')
-    .where({ _empty: { $eq: false }, _id: { $regex: /\d+.*/i } })
-    .sort({ _id: 0 })
-    // .only(['title', 'tags', 'date', 'description', '_path'])
-    .find(),
-)
 </script>
 
 <template>
@@ -27,38 +19,40 @@ const { data: posts } = await useAsyncData('post-list', () =>
       </ContentQuery>
     </div>
     <div flex flex-col>
-      <div
-        v-for="post in posts"
-        :key="post.title"
-        flex flex-col gap-3
-        not-last:border-b-1 not-last-pb-6 not-first-pt-6
-      >
-        <h2>
-          <NuxtLink
-            :to="post._path"
-            main-link
-            font-600 text-xl
-            leading-relaxed sm:leading-loose
-          >
-            {{ post.title }}
-          </NuxtLink>
-        </h2>
-        <div flex gap-3>
-          <div flex items-center gap-1>
-            <div i-iconoir-calendar inline-flex />
-            <span>{{ formatDate(post.date) }}</span>
+      <ContentList v-slot="{ list: posts }" path="posts" :where="{ _empty: { $eq: false }, _id: { $regex: /\d+.*/i } }" :sort="{ _id: -1 }">
+        <div
+          v-for="post in posts"
+          :key="post.title"
+          flex flex-col gap-3
+          not-last:border-b-1 not-last-pb-6 not-first-pt-6
+        >
+          <h2>
+            <NuxtLink
+              :to="post._path"
+              main-link
+              font-600 text-xl
+              leading-relaxed sm:leading-loose
+            >
+              {{ post.title }}
+            </NuxtLink>
+          </h2>
+          <div flex gap-3>
+            <div flex items-center gap-1>
+              <div i-iconoir-calendar inline-flex />
+              <span>{{ formatDate(post.date) }}</span>
+            </div>
+            <div flex items-center gap-1>
+              <div i-iconoir-wristwatch inline-flex />
+              <span inline sm:hidden>{{ `${post.ttr}'` }}</span>
+              <span sm:inline hidden>{{ `${post.ttr} min read` }}</span>
+            </div>
           </div>
-          <div flex items-center gap-1>
-            <div i-iconoir-wristwatch inline-flex />
-            <span inline sm:hidden>{{ `${post.ttr}'` }}</span>
-            <span sm:inline hidden>{{ `${post.ttr} min read` }}</span>
+          <div inline-flex gap-2 flex-wrap>
+            <TechTag v-for="tag in post.tags" :key="tag" :tech="tag" />
           </div>
+          <p>{{ `${post.description.slice(0, 160)}…` }}</p>
         </div>
-        <div inline-flex gap-2 flex-wrap>
-          <TechTag v-for="tag in post.tags" :key="tag" :tech="tag" />
-        </div>
-        <p>{{ `${post.description.slice(0, 160)}…` }}</p>
-      </div>
+      </ContentList>
     </div>
   </div>
 </template>
