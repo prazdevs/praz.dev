5b0bd8986630eca332c89b39767ba5eff44a0af1
prazdevs <sachabouillez@gmail.com>
2022-09-08 01:35:20 +0200

    fix: lint

--- components/Metadata.vue
+++ components/Metadata.vue
@@ -9,29 +9,29 @@ const props = withDefaults(
     title: '',
     description: 'Pragmatic developer, avid learner and frontend nerd.',
     image: 'https://praz.dev/imgs/banner.png',
-  }
+  },
 )
 
 const route = useRoute()
 const url = computed(() => `https://praz.dev${route.path}`)
 const title = computed(() =>
-  props.title ? `${props.title} · praz.dev` : 'praz.dev'
+  props.title ? `${props.title} · praz.dev` : 'praz.dev',
 )
 </script>
 
 <template>
   <Head>
-    <!--? generic metadata -->
+    <!-- ? generic metadata -->
     <Title>{{ title }}</Title>
     <Meta name="description" :content="description" />
-    <!--? opengraph metadata -->
+    <!-- ? opengraph metadata -->
     <Meta name="og:title" :content="title" />
     <Meta name="og:description" :content="description" />
     <Meta name="og:image" :content="image" />
     <Meta name="og:url" :content="url" />
     <Meta name="og:type" content="website" />
     <Meta name="og:locale" content="en" />
-    <!--? twitter metadata -->
+    <!-- ? twitter metadata -->
     <Meta name="twitter:title" :content="title" />
     <Meta name="twitter:description" :content="description" />
     <Meta name="twitter:url" :content="url" />
