3a7df61a8c3457a8d324b63f2a9ccfbc8095fdf0
prazdevs <1631886+prazdevs@users.noreply.github.com>
2022-11-17 20:00:19 +0100

    feat: robots module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/content',
     '@nuxt/image-edge',
+    '@nuxtjs/robots',
     '@nuxtjs/color-mode',
     '@pinia/nuxt',
     '@unocss/nuxt',
