845f27583d0669fe00070a639f8b1916f5950ef4
prazdevs <1631886+prazdevs@users.noreply.github.com>
2022-11-30 00:02:26 +0100

    revert: w/e i'll wait for the stable version

--- components/content/Image.vue
+++ components/content/Image.vue
@@ -4,7 +4,7 @@ withDefaults(defineProps<{ src: string; alt?: string }>(), { alt: "" });
 
 <template>
   <figure flex flex-col items-center justify-center my-7>
-    <NuxtImg
+    <img
       w-full
       max-w-600px
       :src="`/content/${src}`"
