56dafc75d1bb64ca2a5234cb118f3853d4343b78
prazdevs <1631886+prazdevs@users.noreply.github.com>
2023-06-24 14:32:26 +0200

    chore: update deps (remove image temporarily)

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <ContentQuery v-slot="{ data }" path="home" find="one">
       <div flex flex-col sm:flex-row gap-4 justify-center items-center>
         <Metadata />
-        <NuxtPicture
+        <img
           alt="Picture of me, Sacha, with red hair and glasses"
           src="/imgs/sacha.jpg"
           height="300px"
