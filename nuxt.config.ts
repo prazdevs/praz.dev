28477c855d12ddbc7e7cd5042ebe6bbae4fcce2c
prazdevs <1631886+prazdevs@users.noreply.github.com>
2023-08-07 22:52:37 +0200

    chore: disable payload extraction

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ export default defineNuxtConfig({
   devtools: {
     enabled: true,
   },
+  experimental: {
+    payloadExtraction: false,
+  },
   modules: [
     '@nuxt/content',
     '@nuxt/image',
