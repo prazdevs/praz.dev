674bb56d23a8e3bbae108e90b69a50bfdce3a2de
prazdevs <1631886+prazdevs@users.noreply.github.com>
2023-08-07 23:41:52 +0200

    fix: query from script

--- pages/posts/[slug].vue
+++ pages/posts/[slug].vue
@@ -1,33 +1,36 @@
+<script setup lang="ts">
+const route = useRoute()
+const data = await queryContent(route.path).findOne()
+</script>
+
 <template>
   <div>
-    <ContentQuery v-slot="{ data }" :path="$route.path" find="one">
-      <Metadata :title="data.title" />
-      <h1 text-4xl text-center font-600 mb-3 mt-4 sm:mt-7>
-        {{ data.title }}
-      </h1>
-      <div
-        flex
-        justify-between
-        border-t-2
-        border-primary
-        text-lg
-        m-auto
-        mb-8
-        pt-2
-        px-3
-        u:w="auto lg:80%"
-      >
-        <div inline-flex items-center gap-2>
-          <span inline-flex i-iconoir-calendar />
-          <span>{{ useDate(data.date) }}</span>
-        </div>
-        <div flex items-center gap-2>
-          <div i-iconoir-wristwatch inline-flex />
-          <span lt-sm:inline sm:hidden>{{ `${data.ttr}'` }}</span>
-          <span lt-sm:hidden sm:inline>{{ `${data.ttr} min read` }}</span>
-        </div>
+    <Metadata :title="data.title" />
+    <h1 text-4xl text-center font-600 mb-3 mt-4 sm:mt-7>
+      {{ data.title }}
+    </h1>
+    <div
+      flex
+      justify-between
+      border-t-2
+      border-primary
+      text-lg
+      m-auto
+      mb-8
+      pt-2
+      px-3
+      u:w="auto lg:80%"
+    >
+      <div inline-flex items-center gap-2>
+        <span inline-flex i-iconoir-calendar />
+        <span>{{ useDate(data.date) }}</span>
       </div>
-      <ContentRenderer :value="data" />
-    </ContentQuery>
+      <div flex items-center gap-2>
+        <div i-iconoir-wristwatch inline-flex />
+        <span lt-sm:inline sm:hidden>{{ `${data.ttr}'` }}</span>
+        <span lt-sm:hidden sm:inline>{{ `${data.ttr} min read` }}</span>
+      </div>
+    </div>
+    <ContentRenderer :value="data" />
   </div>
 </template>
