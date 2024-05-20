6ab15dc2933bdff501652c9d0a861540918cd88c
Prazdevs <hi@praz.dev>
2024-05-20 15:40:49 +0200

    feat: setup document-driven 404

---
+++ components/DocumentDrivenNotFound.vue
@@ -0,0 +1,47 @@
+<script setup>
+useHeadSafe({ title: '404' })
+</script>
+
+<template>
+  <div class="not-found">
+    <h1 class="title">
+      Uh oh... this page doesn't exist!
+    </h1>
+    <NotFound class="image" />
+    <ContentLink class="link" to="/">
+      Bring me home
+    </ContentLink>
+  </div>
+</template>
+
+<style scoped>
+.not-found {
+  text-align: center;
+}
+
+.title {
+  color: var(--color-accent);
+  font-family: var(--font-family-heading);
+  font-size: var(--font-size-4xl);
+  font-weight: var(--font-weight-bold);
+  line-height: 1.15;
+  margin-top: 2.25rem;
+  margin-bottom: 1rem;
+}
+
+.image {
+  width: 100%;
+  height: 100%;
+  margin: auto;
+}
+
+.link {
+  font-size: var(--font-size-2xl);
+}
+
+@media screen and (min-width: 640px) {
+  .image {
+    width: 75%;
+  }
+}
+</style>
